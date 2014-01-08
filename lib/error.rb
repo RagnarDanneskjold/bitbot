34668c3ef89c7d1cf4952eaec70933784e3a4808
dasixi <i@tomlion.com>
2014-01-09 01:07:54 +0800

    Adjust errors class inheritance

--- lib/error.rb
+++ lib/error.rb
@@ -2,8 +2,8 @@ module BitBot
   class Error < StandardError; end
   class UnknowError < Error; end
   class BalanceError < Error; end
-  class InsufficientMoneyError < Error; end
-  class InsufficientCoinError < Error; end
+  class InsufficientMoneyError < BalanceError; end
+  class InsufficientCoinError < BalanceError; end
   class OrderNotFoundError < Error; end
   class InvalidParamsError < Error; end
   class InvalidPriceError < InvalidParamsError; end
